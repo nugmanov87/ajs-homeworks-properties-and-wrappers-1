d58d481dfa3dc49d58716a8c30b68223ccf0c8c0
Ruslan <ruslan-nugmanov87@mail.ru>
2019-12-08 21:29:13 +0500

    1 задание 7 версия

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,41 @@
-const obj = {
-  name: 'мечник',
-  health: 10,
-  level: 2,
-  attack: 80,
-  defence: 40,
-};
+/* eslint-disable array-callback-return */
+/* eslint-disable consistent-return */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-plusplus */
+/* eslint-disable-next-line array-callback-return */
+/* eslint-disable guard-for-in */
+export default function orderByProps(obj, order) {
+  const newOrderArray = [];
 
-export default function orderByProps(objInput, sortValue) {
-  const arrObj = [];
-  for (const key in objInput) {
-    if (Object.prototype.hasOwnProperty.call(objInput, key)) {
-      arrObj.push({
-        key,
-        value: objInput[key],
+  for (let i = 0; i < order.length; i++) {
+    if (order[i] in obj) {
+      newOrderArray.push({
+        key: order[i],
+        value: `${obj[order[i]]}`,
       });
+
+      const deleteditem = order[i];
+      delete obj[deleteditem];
     }
   }
 
-  for (let i = sortValue.length - 1; i > -1; i -= 1) {
-    arrObj.sort((a) => {
-      if (a.key === sortValue[i]) return -1;
-      return 1;
+  const newArray = [];
+
+  for (const object in obj) {
+    newArray.push({
+      key: `${object}`,
+      value: `${obj[object]}`,
     });
   }
 
-  const arrTemp = arrObj.splice(sortValue.length);
-  arrTemp.sort((a, b) => {
+  newArray.sort((a, b) => {
+    if (a.key < b.key) return -1;
     if (a.key > b.key) return 1;
-    return -1;
   });
-  const arrReturn = arrObj.concat(arrTemp);
-  return arrReturn;
+
+  for (let i = 0; i < newArray.length; i++) {
+    newOrderArray.push(newArray[i]);
+  }
+
+  return newOrderArray;
 }
-console.log(orderByProps(obj, ['name', 'level']));
