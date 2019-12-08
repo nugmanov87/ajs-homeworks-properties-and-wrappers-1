517373ea711f5eff5745d5ba594b7b1e971aa83f
Ruslan <ruslan-nugmanov87@mail.ru>
2019-12-08 20:09:19 +0500

    3 test

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,18 +1,23 @@
-/* eslint-disable linebreak-style */
-import orderByProps from '../app';
+import sort from '../app';
 
-test('Success found', () => {
-  const example = orderByProps({
-    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
-  }, ['name', 'level']);
+test('Sort check', () => {
+  const received = sort(
+    {
+      name: 'мечник',
+      health: 10,
+      level: 2,
+      attack: 80,
+      defence: 40,
+    },
+    ['name', 'level'],
+  );
 
-  const result = [
+  const expected = [
     { key: 'name', value: 'мечник' },
-    { key: 'level', value: '2' },
-    { key: 'attack', value: '80' },
-    { key: 'defence', value: '40' },
-    { key: 'health', value: '10' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
   ];
-
-  expect(example).toEqual(result);
+  expect(received).toEqual(expected);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,44 +1,36 @@
-/* eslint-disable array-callback-return */
-/* eslint-disable consistent-return */
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-plusplus */
-/* eslint-disable-next-line array-callback-return */
-/* eslint-disable guard-for-in */
-export default function orderByProps(obj, order) {
-  const newOrderArray = [];
+const obj = {
+  name: 'мечник',
+  health: 10,
 
-  for (let i = 0; i < order.length; i++) {
-    if (order[i] in obj) {
-      newOrderArray.push({
-        key: order[i],
-        value: `${obj[order[i]]}`,
+  level: 2,
+  attack: 80,
+  defence: 40,
+};
+
+export default function sort(hero, sortValue) {
+  const arrObj = [];
+  for (const key in hero) {
+    if (Object.prototype.hasOwnProperty.call(hero, key)) {
+      arrObj.push({
+        key,
+        value: hero[key],
       });
-
-      const deleteditem = order[i];
-      delete obj[deleteditem];
     }
   }
 
-  const newArray = [];
-
-  for (const object in obj) {
-    newArray.push({
-      key: `${object}`,
-      value: `${obj[object]}`,
+  for (let i = sortValue.length - 1; i > -1; i -= 1) {
+    arrObj.sort((a) => {
+      if (a.key === sortValue[i]) return -1;
+      return 1;
     });
   }
 
-  newArray.sort((a, b) => {
-    if (a.key < b.key) return -1;
+  const arrTemp = arrObj.splice(sortValue.length);
+  arrTemp.sort((a, b) => {
     if (a.key > b.key) return 1;
+    return -1;
   });
-
-  for (let i = 0; i < newArray.length; i++) {
-    newOrderArray.push(newArray[i]);
-  }
-
-  return newOrderArray;
+  const arrReturn = arrObj.concat(arrTemp);
+  return arrReturn;
 }
-// orderByProps({
-//   name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
-// }, ['name', 'level']);
+sort(obj, ['name', 'level']);
